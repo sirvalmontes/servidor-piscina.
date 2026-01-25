23188a508017a28ffca592e4a1f0f90261481eb2
sirvalmontes <75270861+sirvalmontes@users.noreply.github.com>
2026-01-25 17:06:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)   # 👈 TEM que vir ANTES das rotas
+
+memoria = {
+    "nivel": "OK",
+    "bomba": "OFF",
+    "alerta": "NORMAL"
+}
+
+@app.route('/')
+def pagina_inicial():
+    return "SERVIDOR RODANDO"
+
+@app.route('/status', methods=['GET'])
+def ver_status():
+    return jsonify(memoria)
+
 @app.route('/comando', methods=['POST'])
 def receber_comando():
-    dados = request.json
+    dados = request.json or {}
 
     if "nivel" in dados:
         memoria["nivel"] = dados["nivel"]
