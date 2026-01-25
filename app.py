4a4dfbee77e7660e29680f8b3426a146c3329cf8
sirvalmontes <75270861+sirvalmontes@users.noreply.github.com>
2026-01-25 12:45:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -16,7 +15,3 @@ def ver_status():
 @app.route('/comando', methods=['POST'])
 def receber_comando():
     return jsonify({"status": "recebido"})
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
